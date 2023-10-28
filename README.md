b44c20ce13794b7b36a94e839a3069e5938dbd23
manighahrmani <mashapicasso@gmail.com>
2023-10-28 23:11:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Code from Week x
+
+A repository containing the examples from the worksheet.
+
+## Contents
+
+- `practx.py` - A file containing the examples from the worksheet.
+  Copy the contents of this file into yours and run the functions to see what they do.
+- `README.md` - This file.
+- `.gitignore` - A file telling git to ignore certain files (e.g. `.pyc` files).
+  You do not need to worry about this file for now.
---
+++ practx.py
@@ -0,0 +1,40 @@
+def sayHello():
+    print("Hello World")
+
+
+def sayHello2():
+    print("Hello")
+    print("World")
+
+
+def sayBye():
+    print("Goodbye Mars")
+
+
+# TODO: Write `sayBye2` function here
+
+
+# A simple kilograms to ounces conversion program
+# It asks for a weight in kilograms (for example 10)
+# and converts it to ounces (352.74)
+def kilos2Ounces():
+    kilos = float(input("Enter a weight in kilograms: "))
+    ounces = kilos * 35.274
+    print("The weight in ounces is", ounces)
+
+
+def count():
+    for number in range(10):
+        print("Number is now: ", number)
+
+
+# A simple euros to pounds conversion program
+# It asks for a value in euros (for example 10)
+# and converts it to pounds (8.7)
+def euros2Pounds():
+    euros = float(input("Enter a value in euros: "))
+    pounds = euros * 0.87
+    print("The value in pounds is", pounds)
+
+
+# TODO: Write `dollars2Pounds` and the rest of your solutions for the programming exercises here
