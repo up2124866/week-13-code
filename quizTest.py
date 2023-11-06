07a12982b23629bc940c49785b71192f69da84ee
manighahrmani <mashapicasso@gmail.com>
2023-11-06 17:43:00 +0000

    Add quiz in the same file as quizTest

--- quizTest.py
+++ quizTest.py
@@ -1,5 +1,37 @@
 from tkinter import *
-from quiz import Quiz
+
+
+class Question:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+    def getQuestion(self):
+        return self.question
+
+    def checkAnswer(self, userAnswer):
+        return self.answer == userAnswer
+
+
+class Quiz:
+    def __init__(self):
+        self.questions = []
+
+    def addQuestion(self, question, answer):
+        newQuestion = Question(question, answer)
+        self.questions.append(newQuestion)
+
+    def removeQuestionAt(self, index):
+        del self.questions[index]
+
+    def getQuestionAt(self, index):
+        return self.questions[index].getQuestion()
+
+    def checkAnswerAt(self, index, userAnswer):
+        return self.questions[index].checkAnswer(userAnswer)
+
+    def getNumQuestions(self):
+        return len(self.questions)
 
 
 class QuizApp:
@@ -79,12 +111,12 @@ class QuizApp:
                 pady=5
             )
 
-
     def addQuestion(self):
         question = self.newQuestion.get()
         answer = self.newAnswer.get()
         self.quiz.addQuestion(question, answer)
         self.createWidgets()
+
 
 def main():
     quiz = Quiz()
