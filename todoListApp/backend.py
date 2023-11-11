49238b373f7bc407e18bbada3406a8f6448f18ba
manighahrmani <mashapicasso@gmail.com>
2023-11-11 17:25:21 +0000

    Add backend for todo list app

---
+++ todoListApp/backend.py
@@ -0,0 +1,33 @@
+class Task():
+    def __init__(self, msg):
+        self.msg = msg
+
+    def getMsg(self):
+        return self.msg
+
+    def setMsg(self, msg):
+        if len(msg) > 0:
+            self.msg = msg
+
+
+class TaskList():
+    def __init__(self):
+        self.tasks = []
+
+    def addTaskByMsg(self, msg):
+        newTask = Task(msg)
+        self.tasks.append(newTask)
+
+    def getTaskMsgByIndex(self, index):
+        task = self.tasks[index]
+        return task.getMsg()
+
+    def removeTaskAtIndex(self, index):
+        del self.tasks[index]
+
+    def getNumTasks(self):
+        return len(self.tasks)
+
+    def setTaskMsgAtIndex(self, index, msg):
+        task = self.tasks[index]
+        task.setMsg(msg)
